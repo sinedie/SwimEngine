04a46e014f7aa91e2dd6b3c08a4882219ce33e8d
EAR <sinedie@protonmail.com>
2024-01-08 22:57:51 -0500

    add component active attribute

--- src/components/BaseComponent.h
+++ src/components/BaseComponent.h
@@ -4,7 +4,12 @@
 #include <typeinfo>
 
 class BaseComponent {
+protected:
+  bool active = true;
+
 public:
+  bool isActive() { return active; }
+  void setActive(bool active) { this->active = active; }
   virtual const std::type_info *getType() = 0;
 };
 
